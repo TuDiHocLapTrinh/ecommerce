e09d292f1501ce0a23a605cf47e9051a545a869c
AnhTu <anhtudang95@gmail.com>
2024-01-14 22:17:08 +0900

    add CRUD product api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const useRouter = require('./user');
+const productRouter = require('./product');
 const { notFound, errHandler } = require('../middlewares/errHandler');
 
 const initRoutes = (app) => {
   app.use('/api/user', useRouter);
+  app.use('/api/product', productRouter);
 
   // when no correct path notFound Err
   app.use(notFound);
